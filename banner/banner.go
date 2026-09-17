df70a8f5e3daf78733c7985816091c0d097a2ea4
andriu5 <andres.alvear5@gmail.com>
2026-09-17 15:09:51 -0300

    ADD Strings, Unicode lesson. Centres text inside a fixed-width banner.

---
+++ banner/banner.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	banner("Go", 6)
+	banner("G", 6)
+}
+
+func banner(text string, width int) {
+	padding := (width - len(text)) / 2
+	fmt.Print(strings.Repeat(" ", padding))
+	fmt.Println(text)
+	fmt.Println(strings.Repeat("-", width))
+}
+
+// banner("Go", 6)
+//   Go
+// ------
